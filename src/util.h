7d57ae835dbf43ab774751bd40efb3ab352720c2
Coradan <icoradan@gmail.com>
2013-12-31 06:36:09 +0100

    Rename Sleep to Sleep2 in util.h
    Compiling the 8.5.3_irc version I had to rename Sleep function because it is ambiguous. I renamed it in util.h and in irc.cpp from Sleep to Sleep2.

--- src/util.h
+++ src/util.h
@@ -87,7 +87,7 @@ T* alignup(T* p)
 #define strlwr(psz)         to_lower(psz)
 #define _strlwr(psz)        to_lower(psz)
 #define MAX_PATH            1024
-inline void Sleep(int64 n)
+inline void Sleep2(int64 n)
 {
     /*Boost has a year 2038 problem— if the request sleep time is past epoch+2^31 seconds the sleep returns instantly.
       So we clamp our sleeps here to 10 years and hope that boost is fixed by 2028.*/
